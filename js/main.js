d4b46fd98fd5967324d44b641671554cb7aa2004
cKirkktoh <113542793+cKirkktoh@users.noreply.github.com>
2023-12-10 12:28:24 -0500

    added page transitions

--- js/main.js
+++ js/main.js
@@ -19,3 +19,43 @@ window.addEventListener('resize', checkVisibility);
 checkVisibility();
 })();
 
+(() => {
+
+  const tl = gsap.timeline({defaults: {ease: 'power2.out'}});
+
+  tl.to(".slider", {y: "-100%", duration: 1.2})
+  tl.to(".introduction-content", {y: "0%", duration: 1.4}, "-=1")
+  tl.to(".circle-shape", {y: "0%", duration: 1.5}, "-=1")
+  tl.to(".about-me-text", {y: "0%", duration: 1.5}, "-=1")
+  tl.to(".skills-image", {y: "0%", duration: 0.2}, "-=1")
+
+})();
+
+// hotspot script
+(() => {
+  const model = document.querySelector("#model");
+  const hotspots = document.querySelectorAll(".Hotspot");
+
+  function modelLoaded() {
+      hotspots.forEach(hotspot => {
+          hotspot.style.display = "block";
+      });
+  }
+
+  function showInfo() {
+      let selected = document.querySelector(`#${this.slot}`);
+      gsap.to(selected, 1, { autoAlpha: 1 });
+  }
+
+  function hideInfo() {
+      let selected = document.querySelector(`#${this.slot}`);
+      gsap.to(selected, 1, { autoAlpha: 0 });
+  }
+
+  model.addEventListener("load", modelLoaded);
+
+  hotspots.forEach(function (hotspot) {
+      hotspot.addEventListener("mouseover", showInfo);
+      hotspot.addEventListener("mouseout", hideInfo);
+  });
+})();
