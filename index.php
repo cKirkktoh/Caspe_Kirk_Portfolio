c3b7ece5db55c0d1832dc360d423563927b9a193
cKirkktoh <113542793+cKirkktoh@users.noreply.github.com>
2025-02-20 16:04:46 -0500

    s

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="initial-scale=1.0, width=device-width">
-    <title>Projects Gallery</title>
+    <title>My Portfolio</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="preconnect" href="https://fonts.googleapis.com">
